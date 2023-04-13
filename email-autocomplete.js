479e870edf2d4cef3a8364c2dbbee6ee76d4f5bf
Ira Stefan <ira.stefan91@gmail.com>
2023-04-13 13:41:54 +0300

    function onKeyDown

--- email-autocomplete.js
+++ email-autocomplete.js
@@ -2,12 +2,13 @@ class EmailAutocomplete {
     constructor(input, domains) {
         this.input = input;
         this.domains = domains;
+        this.currentFocus = -1;
         this.init();
     }
 
     init() {
-        this.input.addEventListener('input', this.inputEmail.bind(this))
-
+        this.input.addEventListener('input', this.inputEmail.bind(this));
+        this.input.addEventListener('keydown', this.onKeyDown.bind(this));
         document.addEventListener("click", this.closeList.bind(this));
     }
 
@@ -17,6 +18,7 @@ class EmailAutocomplete {
 
     setInputValue(domain) {
         this.input.value = this.input.value.split('@')[0] + '@' + domain;
+        this.closeList();
     }
 
     isEmail(input) {
@@ -28,6 +30,15 @@ class EmailAutocomplete {
 
     getDomain(input) {
         return input.split('@')[1].toLowerCase();
+    }
+
+    getCurrentDomainList() {
+        return this.domains.filter(
+            domain => 
+            domain.slice(0, this.getDomain(this.getInputValue()).length)
+             === 
+            this.getDomain(this.getInputValue()).toLowerCase()
+        );
     }
 
     inputEmail() {
@@ -55,22 +66,53 @@ class EmailAutocomplete {
                     domainList.appendChild(domainItem);
 
                     domainItem.addEventListener('click', () => {
-                        this.setInputValue(domainItem.innerText); 
-                        this.closeList();
+                        this.setInputValue(domainItem.innerText);
                     })
                 }
             });
         }
     }
 
+    onKeyDown(e) {
+        //key down
+        if (e.keyCode == 40) {
+            if (this.currentFocus < this.getCurrentDomainList().length-1) {
+                this.currentFocus++;
+            } else this.currentFocus = 0;
+            this.removeActiveDomain();
+            document.querySelectorAll('#email-domains div')[this.currentFocus].classList.add('active');
+        }
+        //key up
+        if (e.keyCode == 38) { 
+            if (this.currentFocus > 0) {
+                this.currentFocus--;
+                console.log(this.currentFocus)
+            } else this.currentFocus = this.getCurrentDomainList().length-1;
+            this.removeActiveDomain();
+            document.querySelectorAll('#email-domains div')[this.currentFocus].classList.add('active');
+        }
+        //key enter
+        if (e.keyCode == 13) {
+            e.preventDefault();
+            this.setInputValue(this.domains[this.currentFocus]);
+        }
+        
+    }
+
+    removeActiveDomain() {
+        document.querySelectorAll('#email-domains div.active').forEach(item => {
+            item.classList.remove('active')
+        })
+    }
+
     closeList() {
         let domains = document.getElementById('email-domains');
-        if (domains)
-            domains.parentNode.removeChild(domains);
+        if (domains) domains.parentNode.removeChild(domains);
     }
 }
 
 const inputEmail = document.querySelector(".email")
-const domains = ['google.com', 'gmail.com', 'yahoo.com']
+
+const domains = ['google.com', 'gmail.com', 'gmail1.com', 'gmail2.com', 'yahoo.com']
 
 new EmailAutocomplete(inputEmail, domains)
